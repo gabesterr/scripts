da120913786a152871acfe223fc681e646add287
gabesterr <gabesterr@users.noreply.github.com>
2016-05-23 00:22:27 -0500

    Create checkadou.sh
    OS X script checks and reports AD distinguishedName attribute.

---
+++ checkadou.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+processLine(){
+	line="$@"
+  
+ECHO "${line}" -O
+}
+
+if [ "$1" == "" ]; then
+	user=`whoami`
+else
+   user="$1"
+fi
+
+#use dscl in ineractive mode to find out ADdomain
+ADdomain="All\ Domains"
+DOMAINNAME="yourdomain"
+#userRecord=`dscl localhost -read /Active\ Directory/$DOMAINNAME/All\ Domains/Users/$user distinguishedName`
+userRecord=`dscl localhost -read /Active\ Directory/$DOMAINNAME/All\ Domains/Users/$user distinguishedName | sed 's/dsAttrTypeNative:distinguishedName://'`
+echo $user " : " $userRecord
